0bdc01ce05627e47b65d07fc77a839be295f8537
John Park <62269310+shinopark@users.noreply.github.com>
2023-04-18 01:21:55 -0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,17 +1,37 @@
-import { clock } from './clock.js';
+import { currentTime } from './clock.js';
 
 // Call the clock function to get the current time
-const currentTime = clock();
+const currentTime = currentTime();
 
 // Extract the hour from the current time
 const currentHour = currentTime.getHours();
 
-// Define an array of video URLs to cycle through
-const videoURLs = [
-  'https://www.youtube.com/watch?v=9j9YEuFeDAw', //12 AM GameCube
-  'https://www.youtube.com/watch?v=UMMRXzXs-_k', //1 AM
-  'https://www.youtube.com/watch?v=-GS2Uf2koDI'  //2 AM
-];
+// Define an array of video URLs to cycle through for each hour
+const videoURLs = [  'https://www.youtube.com/watch?v=9j9YEuFeDAw', //12 AM GameCube  
+                      'https://www.youtube.com/watch?v=UMMRXzXs-_k', //1 AM  
+                      'https://www.youtube.com/watch?v=-GS2Uf2koDI', //2 AM  
+                      'https://www.youtube.com/watch?v=se7iaIfFdoQ', //3 AM  
+                      'https://www.youtube.com/watch?v=AzHvhnzxeEw', //4 AM  
+                      'https://www.youtube.com/watch?v=1auEF6PRm7Q'  //5 AM
+                      'https://www.youtube.com/watch?v=hhlLbg4CRBc'  //6 AM
+                      'https://www.youtube.com/watch?v=jcPIAICO-DI'  //7 AM
+                      'https://www.youtube.com/watch?v=J22dco5U-nY'  //8 AM
+                      'https://www.youtube.com/watch?v=odevRs61OqY'  //9 AM
+                      'https://www.youtube.com/watch?v=uFq-VvnEa80'  //10 AM
+                      'https://www.youtube.com/watch?v=0dzoz1iOvws'  //11 AM
+                      'https://www.youtube.com/watch?v=4pmZrtO8_co'  //12 PM
+                      'https://www.youtube.com/watch?v=dAABrbS8QeA'  //1 PM
+                      'https://www.youtube.com/watch?v=hwENcKas4nU'  //2 PM
+                      'https://www.youtube.com/watch?v=TMFHHTFEMPQ'  //3 PM
+                      'https://www.youtube.com/watch?v=_m_cI7cssPQ'  //4 PM
+                      'https://www.youtube.com/watch?v=zcIDJd-ncHI'  //5 PM
+                      'https://www.youtube.com/watch?v=7nNsOcPfGxk'  //6 PM
+                      'https://www.youtube.com/watch?v=T_454gyj9PE'  //7 PM
+                      'https://www.youtube.com/watch?v=GlYzXvHQaDw'  //8 PM
+                      'https://www.youtube.com/watch?v=Au8cSsWgYUY'  //9 PM
+                      'https://www.youtube.com/watch?v=nB1xsg-X3-0'  //10 PM
+                      'https://www.youtube.com/watch?v=Ar8sNQNvi90'  //11 PM
+                    ];
 
 // Function to change the video URL
 function changeVideoURL() {
@@ -26,6 +46,50 @@ function changeVideoURL() {
 }
 
 // Call the changeVideoURL function based on the current hour
-if (currentHour >= 0 && currentHour < 1) {
+if (currentHour === 0) {
+  changeVideoURL();
+} else if (currentHour === 1) {
+  changeVideoURL();
+} else if (currentHour === 2) {
+  changeVideoURL();
+} else if (currentHour === 3) {
+  changeVideoURL();
+} else if (currentHour === 4) {
+  changeVideoURL();
+} else if (currentHour === 5) {
+  changeVideoURL();
+} else if (currentHour === 6) {
+  changeVideoURL();
+} else if (currentHour === 7) {
+  changeVideoURL();
+} else if (currentHour === 8) {
+  changeVideoURL();
+} else if (currentHour === 9) {
+  changeVideoURL();
+} else if (currentHour === 10) {
+  changeVideoURL();
+} else if (currentHour === 11) {
+  changeVideoURL();
+} else if (currentHour === 12) {
+  changeVideoURL();
+} else if (currentHour === 13) {
+  changeVideoURL();
+} else if (currentHour === 14) {
+  changeVideoURL();
+} else if (currentHour === 15) {
+  changeVideoURL();
+} else if (currentHour === 16) {
+  changeVideoURL();
+} else if (currentHour === 17) {
+  changeVideoURL();
+} else if (currentHour === 18) {
+  changeVideoURL();
+} else if (currentHour === 19) {
+  changeVideoURL();
+} else if (currentHour === 21) {
+  changeVideoURL();
+} else if (currentHour === 22) {
+  changeVideoURL();
+} else if (currentHour === 23) {
   changeVideoURL();
 }
