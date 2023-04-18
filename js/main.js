f55dc834f6d055d0112cc84271aada0e37e9db41
John Park <62269310+shinopark@users.noreply.github.com>
2023-04-18 04:33:53 -0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -28,41 +28,46 @@
 
   // Define an array of video URLs to cycle through for each hour
   const videoURLs = [
-    'https://www.youtube.com/watch?v=9j9YEuFeDAw', //12 AM GameCube  
-    'https://www.youtube.com/watch?v=UMMRXzXs-_k', //1 AM  
-    'https://www.youtube.com/watch?v=-GS2Uf2koDI', //2 AM  
-    'https://www.youtube.com/watch?v=se7iaIfFdoQ', //3 AM  
-    'https://www.youtube.com/watch?v=AzHvhnzxeEw', //4 AM  
-    'https://www.youtube.com/watch?v=1auEF6PRm7Q',  //5 AM
-    'https://www.youtube.com/watch?v=hhlLbg4CRBc',  //6 AM
-    'https://www.youtube.com/watch?v=jcPIAICO-DI',  //7 AM
-    'https://www.youtube.com/watch?v=J22dco5U-nY',  //8 AM
-    'https://www.youtube.com/watch?v=odevRs61OqY',  //9 AM
-    'https://www.youtube.com/watch?v=uFq-VvnEa80',  //10 AM
-    'https://www.youtube.com/watch?v=0dzoz1iOvws',  //11 AM
-    'https://www.youtube.com/watch?v=4pmZrtO8_co',  //12 PM
-    'https://www.youtube.com/watch?v=dAABrbS8QeA',  //1 PM
-    'https://www.youtube.com/watch?v=hwENcKas4nU',  //2 PM
-    'https://www.youtube.com/watch?v=TMFHHTFEMPQ',  //3 PM
-    'https://www.youtube.com/watch?v=_m_cI7cssPQ',  //4 PM
-    'https://www.youtube.com/watch?v=zcIDJd-ncHI',  //5 PM
-    'https://www.youtube.com/watch?v=7nNsOcPfGxk',  //6 PM
-    'https://www.youtube.com/watch?v=T_454gyj9PE',  //7 PM
-    'https://www.youtube.com/watch?v=GlYzXvHQaDw',  //8 PM
-    'https://www.youtube.com/watch?v=Au8cSsWgYUY',  //9 PM
-    'https://www.youtube.com/watch?v=nB1xsg-X3-0',  //10 PM
-    'https://www.youtube.com/watch?v=Ar8sNQNvi90'  //11 PM
-
+    {hour: 0, url:'https://www.youtube.com/watch?v=9j9YEuFeDAw'}, //12 AM GameCube  
+    {hour: 1, url:'https://www.youtube.com/watch?v=UMMRXzXs-_k'}, //1 AM  
+    {hour: 2, url:'https://www.youtube.com/watch?v=-GS2Uf2koDI'}, //2 AM  
+    {hour: 3, url:'https://www.youtube.com/watch?v=se7iaIfFdoQ'}, //3 AM  
+    {hour: 4, url:'https://www.youtube.com/watch?v=AzHvhnzxeEw'}, //4 AM  
+    {hour: 5, url:'https://www.youtube.com/watch?v=1auEF6PRm7Q'},  //5 AM
+    {hour: 6, url:'https://www.youtube.com/watch?v=hhlLbg4CRBc'},  //6 AM
+    {hour: 7, url:'https://www.youtube.com/watch?v=jcPIAICO-DI'},  //7 AM
+    {hour: 8, url:'https://www.youtube.com/watch?v=J22dco5U-nY'},  //8 AM
+    {hour: 9, url:'https://www.youtube.com/watch?v=odevRs61OqY'},  //9 AM
+    {hour: 10, url:'https://www.youtube.com/watch?v=uFq-VvnEa80'},  //10 AM
+    {hour: 11, url:'https://www.youtube.com/watch?v=0dzoz1iOvws'},  //11 AM
+    {hour: 12, url:'https://www.youtube.com/watch?v=4pmZrtO8_co'},  //12 PM
+    {hour: 13, url:'https://www.youtube.com/watch?v=dAABrbS8QeA'},  //1 PM
+    {hour: 14, url:'https://www.youtube.com/watch?v=hwENcKas4nU'},  //2 PM
+    {hour: 15, url:'https://www.youtube.com/watch?v=TMFHHTFEMPQ'},  //3 PM
+    {hour: 16, url:'https://www.youtube.com/watch?v=_m_cI7cssPQ'},  //4 PM
+    {hour: 17, url:'https://www.youtube.com/watch?v=zcIDJd-ncHI'},  //5 PM
+    {hour: 18, url:'https://www.youtube.com/watch?v=7nNsOcPfGxk'},  //6 PM
+    {hour: 19, url:'https://www.youtube.com/watch?v=T_454gyj9PE'},  //7 PM
+    {hour: 20, url:'https://www.youtube.com/watch?v=GlYzXvHQaDw'},  //8 PM
+    {hour: 21, url:'https://www.youtube.com/watch?v=Au8cSsWgYUY'},  //9 PM
+    {hour: 22, url:'https://www.youtube.com/watch?v=nB1xsg-X3-0'},  //10 PM
+    {hour: 23, url:'https://www.youtube.com/watch?v=Ar8sNQNvi90'}  //11 PM
   ];
 
   // Keep track of the current index
-  let currentIndex = 0;
+  // let currentIndex = 0;
 
   // Function to change the video URL based on the current hour
   function changeVideoURL() {
+    const now = new Date();
+    const hour = now.getHours();
     const videoIframe = document.querySelector('.video iframe');
-    videoIframe.src = videoURLs[currentIndex % videoURLs.length];
-    currentIndex++;
+    const videoURL = videoURLs.find(entry => entry.hour === hour)?.url;
+    if (videoURL) {
+      videoIframe.src = videoURL;
+    }
+    // videoIframe.src = videoURLs[currentIndex % videoURLs.length];
+    // currentIndex++;
   }
 
   // Get the div element where the video player will be inserted
@@ -77,7 +82,7 @@
   videoIframe.allowFullscreen = true;
 
   // Set the initial video URL
-  videoIframe.src = 'https://www.youtube.com/embed/zcIDJd-ncHI';
+  videoIframe.src = ;
 
   // Insert the video iframe element into the div element
   videoPlayerDiv.appendChild(videoIframe);
@@ -95,7 +100,6 @@
 
 
 
-
 // Function to change the video URL
 // function changeVideoURL(currentHour) {
 //   // Get the video iframe element
@@ -109,3 +113,8 @@
 // }
 
 
+// function changeVideoURL() {
+//   const videoIframe = document.querySelector('.video iframe');
+//   videoIframe.src = videoURLs[currentIndex % videoURLs.length];
+//   currentIndex++;
+// }
